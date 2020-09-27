b2eccf3ea26d12b239bbe24950ee1556a960536c
Pushkar Anand <anandpushkar088@gmail>
2020-09-28 01:39:53 +0530

    Add support for Docker.

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"context"
 	"github.com/go-redis/redis/v8"
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"os"
 	"strconv"
@@ -20,11 +20,6 @@ func main() {
 
 	logger.SetLevel(logrus.TraceLevel)
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		logger.WithError(err).Panic("Could not load env")
-	}
-
 	redisDB, err := strconv.Atoi(os.Getenv(envRedisDB))
 	if err != nil {
 		logger.WithError(err).Panic("REDIS_DB must be an integer")
@@ -35,6 +30,13 @@ func main() {
 		Password: os.Getenv(envRedisPass),
 		DB:       redisDB,
 	})
+
+	_, err = client.Ping(context.Background()).Result()
+	if err != nil {
+		logger.WithError(err).Panic("Error connecting to redis")
+	}
+
+	logger.Infof("Connected with redis: %s", client)
 
 	server := NewServer(logger, client)
 
